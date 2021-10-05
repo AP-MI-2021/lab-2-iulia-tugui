eff52aa96d9c5c7baf48bada3b8c51248409a0cb
Tugui Iulia <iulia.tugui@stud.ubbcluj.ro>
2021-10-05 08:08:59 +0300

    Problemele 5,6,7

--- main.py
+++ main.py
@@ -26,19 +26,61 @@ returneaza True daca numarul este superprim sau False in caz contrar
 def is_superprime(n):
     if n<2:
         return False
+        #daca numarul este mai mic decat 2 el nu este prim
     else:
         while (n>0):
+            #cat timp numarul este diferit de 0 îi extragem prefixele
             for i in range (2,n//2+1):
                 if n%i==0:
                     return False
+                #dacă găsim un prefix care nu este prim se returnează False pentru tot numărul
             n = n//10
         return True
+    #dacă nu am găsit niciun prefix care să nu fie neprim înseamnă că numărul este superprim și se returnează True
 
+def test_is_superprime():
+    assert is_superprime(233) is True
+    assert is_superprime(237) is False
 
+'''
+Problema 7: Determină dacă un număr este antipalindrom
+returneaza True daca numarul este antipalindrom sau False in caz contrar
+'''
+def is_antipalindrome(n):
+    copie_n = n
+    #fac o copie a numarului n pentru a putea compara cifrele egal departate ale numarului cu cele ale inversului sau
+    invers = 0
+    contor_cifre = 0
+    while (n>0):
+        ultima_cifra = n%10
+        invers = invers*10 + ultima_cifra
+        n = n//10
+        contor_cifre = contor_cifre +1
+        #contorizez numarul de cifre
+    contor_cifre = contor_cifre //2
+    #accesez cifrele doar pana la jumatatea numarului
+    while (contor_cifre >0):
+        if(copie_n%10==invers%10):
+            return False
+        copie_n = copie_n //10
+        invers = invers //10
+        contor_cifre = contor_cifre - 1
+    return True
+
+def test_is_antipalindrome():
+    assert is_antipalindrome(2783) is True
+    assert is_antipalindrome(27583) is True
+    assert is_antipalindrome(2773) is False
+    assert is_antipalindrome(27573) is False
+    assert is_antipalindrome(321) is True
 
 def main():
     print(is_palindrome(1))
+    test_is_palindrome()
     print(is_superprime(237))
+    test_is_superprime()
+    print(is_antipalindrome(27583))
+    test_is_antipalindrome()
 
 if __name__ == "__main__":
     main()
